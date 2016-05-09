f89e032a62cad065e7f263ccb20280a9adbed5f5
Sojex <josevidal336@yahoo.com>
2016-05-09 13:06:02 -0400

    Venture Island Prototype
    Newest prototype of Venture Island

---
+++ Assets/PLayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLayerMovement : MonoBehaviour {
+
+	private  float speed=15f;
+	public GameObject camera;
+	private Rigidbody rb;
+	Vector3 force;
+
+
+
+	void Start(){
+	
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+
+
+	void Update () {
+
+		//Movement Code that utalizes the input function in unity referencing the horizontal to manipulate the x value on the players position and the veticle for the z
+
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		Vector3 moveX = transform.right * x;
+		Vector3 moveZ = transform.forward * z;
+
+
+		 force = (moveX + moveZ).normalized * speed;  
+
+
+	
+
+
+
+
+
+
+
+
+	}
+
+
+	void FixedUpdate() {
+		//Was going to use "transform.Translate (force);" Until I saw I was going through other objects
+
+		rb.MovePosition (rb.position + force * Time.deltaTime);
+		
+
+		
+	}
+
+
+
+}
---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour {
+
+
+
+	public float lookSensitivity=5f;
+	public float xRot;
+	public float yRot;
+	public float current_xRot;
+	public float current_yRot;
+	public float smoothness= .1f;
+	private float xRotV;
+	private float yRotV;
+
+	// Update is called once per frame
+	void Update () {
+
+	
+
+		//Player rotation code took me a while to realize that the mouse y would inflounce the horizontal rotation and vice versa
+
+		xRot-= Input.GetAxis ("Mouse Y") * lookSensitivity;
+		yRot+= Input.GetAxis ("Mouse X") * lookSensitivity;
+
+
+
+		/*prevents player from doing backflips while looking up got code from youtube channel called penguin design
+			limits the value of the xRot which essentially just limits how high up and low the player may look
+		*/
+
+		xRot = Mathf.Clamp (xRot, -80, 100);
+
+
+
+
+
+		//code I found from a panguin design tutorial to make the rotation smoother 
+
+	//	current_xRot = Mathf.SmoothDamp (current_xRot, xRot, xRotV, smoothness);
+
+
+
+
+		//actually does the rotation based off of xRot and yRot variables
+
+
+
+		transform.rotation = Quaternion.Euler (0f, yRot, 0f);
+		Camera.main.transform.localRotation= Quaternion.Euler (xRot,0f,0f);
+
+
+		//Awesome code that hides cursor and locks it in the middle of the screen helps a lot with rotation
+		Screen.lockCursor = true;
+
+	}
+}
